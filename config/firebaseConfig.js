e0c9f500218be3906fbf357fbdc5cfeeee0d00b7
garrychiao <g50905g@gmail.com>
2018-07-17 04:51:12 +0800

    vuely with firebase auth

---
+++ config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAUDh05aFmIKuck6mLTXGbbm35UQ4UahDc",
+  authDomain: "tsf-online.firebaseapp.com",
+  databaseURL: "https://tsf-online.firebaseio.com",
+  projectId: "tsf-online",
+  storageBucket: "tsf-online.appspot.com",
+  messagingSenderId: "72344349047"
+}
